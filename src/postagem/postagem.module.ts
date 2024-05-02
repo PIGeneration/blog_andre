84525d49643157d8e0d569588febee160296e920
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-05-02 03:24:52 -0300

    Blog

---
+++ src/postagem/postagem.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostagemController } from './controllers/postagem.controller';
+import { Postagem } from './entities/postagem.entity';
+import { PostagemService } from './services/postagem.service';
+import { TemaService } from '../tema/services/tema.service';
+import { TemaModule } from '../tema/tema.module';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Postagem]), TemaModule],
+  providers: [PostagemService, TemaService],
+  controllers: [PostagemController],
+  exports: [TypeOrmModule],
+})
+export class PostagemModule {}
